b40a46bc928cf96527c708a623beeecda7c0335f
Wojciech Dąbrowski <wojtasop@gmail.com>
2018-03-23 02:20:37 +0100

    Removed redundant namespace qualifier.

--- SolidPrinciples/SolidPrinciples.Srp/Step0/EmployeeHelper.cs
+++ SolidPrinciples/SolidPrinciples.Srp/Step0/EmployeeHelper.cs
@@ -4,13 +4,13 @@ namespace SolidPrinciples.Srp.Step0
 {
     public static class EmployeeHelper
     {
-        public static decimal CalculatePayment(Step0.Employee employee)
+        public static decimal CalculatePayment(Employee employee)
         {
             // ...
             throw new NotImplementedException();
         }
 
-        public static string GetEmployeeDescription(Step0.Employee employee)
+        public static string GetEmployeeDescription(Employee employee)
         {
             // ...
             throw new NotImplementedException();
